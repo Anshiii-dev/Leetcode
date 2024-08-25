2ad4fa7a6eb13df32c53ab6106f616f72593fc23
Anshiii-dev <anssaleem504@gmail.com>
2024-08-25 22:08:44 +0500

    Time: 173 ms (20.52%), Space: 14 MB (7.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,20 +1,17 @@
 class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        j=0
-        count=0
-        for i in range(len(nums)):
-            if nums[j]==nums[i]:
+   def majorityElement(self, nums):
+       """
+       :type nums: List[int]
+       :rtype: int
+       """
+       j=0
+       count=0
+       for i in range(len(nums)):
+           if nums[j]==nums[i]:
                 count+=1
-                if count>1:
-                    return nums[j]
-                    break
-        
-        
-
-
-
-        
+           else:
+                count-=1
+           if count==0:
+                j=i
+                count=1
+       return int(nums[j])
