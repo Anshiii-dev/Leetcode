efd50fa6b3f589e12c3c383e5ed4692865b710d5
Anshiii-dev <anssaleem504@gmail.com>
2024-08-26 03:01:34 +0500

    Time: 14 ms (88.67%), Space: 11.6 MB (38.14%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,15 +7,6 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        
-        dummy=[0]*(m+n)
-        for i in range(len(dummy)):
-         if i<m:
-          dummy[i]=nums1[i]
-         elif i>m-1:
-          dummy[i]=nums2[len(dummy)-i-1]
-        for i in range(len(nums1)): 
-          nums1[i]=dummy[i]
-        nums1.sort()
-        
-        
+        for i in range(m,m+n):
+            nums1[i]=nums2[i-m]
+        return nums1.sort()
