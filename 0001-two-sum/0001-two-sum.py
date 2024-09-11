379849ae8c2ebd028fc85f6b597c0cbb0f3c62c8
Anshiii-dev <anssaleem504@gmail.com>
2024-09-11 15:49:57 +0500

    Time: 2161 ms (27.45%), Space: 12.6 MB (17.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,15 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        index=[]
         for i in range(len(nums)):
-            for j in range(1,len(nums)):
-                if nums[i]+nums[j]==target:
-                  if i or j not in index:
-                    if i!=j:
-                      index.append(i)
-                      index.append(j)
-                      return index 
-                      break
-                   
-                    
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
