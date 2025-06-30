33ac38c79c39888f43f9fb3bbc59603f1559ccad
Anshiii-dev <anssaleem504@gmail.com>
2025-07-01 02:53:40 +0500

    Time: 4 ms (27.85%), Space: 12.5 MB (83.86%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,9 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        prev = None
-        while prev != s:
-            prev = s
-            s = s.replace("()", "").replace("{}", "").replace("[]", "")
+        stack = []
+        bracket_map = {')': '(', ']': '[', '}': '{'}
+        for char in s:
+            if char in bracket_map:
+                top=stack.pop() if  stack else '#'
+                if bracket_map[char] !=top:
+                    return False
+            else:
+                    stack.append(char)
         
-        return s == ""
+        return not stack
+
+
+       
